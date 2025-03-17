a171d1389111e6b24a61f7cfbb92fc4ae4fb7f9a
Peyman Naderi <peymannaderi10>
2025-03-16 23:17:13 -0400

    configured for github pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Enable static exports
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: '/barber-website', // Replace with your repository name
+}
+
+module.exports = nextConfig 
